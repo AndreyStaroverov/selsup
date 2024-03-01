a1df495c984cba32215c3272499b790070bdd954
Andrey <fanatSai@yandex.ru>
2024-03-02 02:07:46 +0300

    1.0

---
+++ src/main/java/org/example/CrptApi.java
@@ -0,0 +1,295 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class CrptApi {
+    private final HttpClient httpClient;
+    private final Gson gson;
+    private final TimeUnit timeUnit;
+    private final int requestLimit;
+    private long lastRequest;
+    private int requestCount;
+
+    public CrptApi(TimeUnit timeUnit, int requestLimit) {
+        this.httpClient = HttpClient.newHttpClient();
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+        this.timeUnit = timeUnit;
+        this.requestLimit = requestLimit;
+        this.lastRequest = System.currentTimeMillis();
+        this.requestCount = 0;
+    }
+
+    public synchronized void createDocument(Document document, String signature) throws IOException, InterruptedException {
+        long currentTime = System.currentTimeMillis();
+        long checkTime = currentTime - lastRequest;
+        long timeInterval = timeUnit.toMillis(1);
+
+        if (checkTime < timeInterval && requestCount >= requestLimit) {
+            long sleepTime = timeInterval - checkTime;
+            TimeUnit.MILLISECONDS.sleep(sleepTime);
+        }
+
+        String requestBody = gson.toJson(new DocumentRequest(document, signature));
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://ismp.crpt.ru/api/v3/lk/documents/create"))
+                .timeout(Duration.ofSeconds(30))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (currentTime - lastRequest >= timeInterval) {
+            requestCount = 1;
+        } else {
+            requestCount++;
+        }
+        lastRequest = System.currentTimeMillis();
+    }
+
+    static class DocumentRequest {
+        private final Document description;
+        private final String signature;
+
+        public DocumentRequest(Document description, String signature) {
+            this.description = description;
+            this.signature = signature;
+        }
+    }
+
+    static class Document {
+
+        private String description;
+        private String docId;
+        private String docStatus;
+        private String docType;
+        private boolean importRequest;
+        private String ownerInn;
+        private String participantInn;
+        private String producerInn;
+        private String productionDate;
+        private String productionType;
+        private List<Product> products;
+        private String regDate;
+
+        public Document(String description, String docId, String docStatus, String docType, boolean importRequest,
+                        String ownerInn, String participantInn, String producerInn, String productionDate,
+                        String productionType, List<Product> products, String regDate, String regNumber) {
+            this.description = description;
+            this.docId = docId;
+            this.docStatus = docStatus;
+            this.docType = docType;
+            this.importRequest = importRequest;
+            this.ownerInn = ownerInn;
+            this.participantInn = participantInn;
+            this.producerInn = producerInn;
+            this.productionDate = productionDate;
+            this.productionType = productionType;
+            this.products = products;
+            this.regDate = regDate;
+            this.regNumber = regNumber;
+        }
+
+        private String regNumber;
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getDocId() {
+            return docId;
+        }
+
+        public void setDocId(String docId) {
+            this.docId = docId;
+        }
+
+        public String getDocStatus() {
+            return docStatus;
+        }
+
+        public void setDocStatus(String docStatus) {
+            this.docStatus = docStatus;
+        }
+
+        public String getDocType() {
+            return docType;
+        }
+
+        public void setDocType(String docType) {
+            this.docType = docType;
+        }
+
+        public boolean isImportRequest() {
+            return importRequest;
+        }
+
+        public void setImportRequest(boolean importRequest) {
+            this.importRequest = importRequest;
+        }
+
+        public String getOwnerInn() {
+            return ownerInn;
+        }
+
+        public void setOwnerInn(String ownerInn) {
+            this.ownerInn = ownerInn;
+        }
+
+        public String getParticipantInn() {
+            return participantInn;
+        }
+
+        public void setParticipantInn(String participantInn) {
+            this.participantInn = participantInn;
+        }
+
+        public String getProducerInn() {
+            return producerInn;
+        }
+
+        public void setProducerInn(String producerInn) {
+            this.producerInn = producerInn;
+        }
+
+        public String getProductionDate() {
+            return productionDate;
+        }
+
+        public void setProductionDate(String productionDate) {
+            this.productionDate = productionDate;
+        }
+
+        public String getProductionType() {
+            return productionType;
+        }
+
+        public void setProductionType(String productionType) {
+            this.productionType = productionType;
+        }
+
+        public List<Product> getProducts() {
+            return products;
+        }
+
+        public void setProducts(List<Product> products) {
+            this.products = products;
+        }
+
+        public String getRegDate() {
+            return regDate;
+        }
+
+        public void setRegDate(String regDate) {
+            this.regDate = regDate;
+        }
+
+        public String getRegNumber() {
+            return regNumber;
+        }
+
+        public void setRegNumber(String regNumber) {
+            this.regNumber = regNumber;
+        }
+    }
+
+    static class Product {
+
+        private String certificateDocument;
+        private String certificateDocumentDate;
+        private String certificateDocumentNumber;
+        private String ownerInn;
+        private String producerInn;
+        private String productionDate;
+        private String tnvedCode;
+        private String uitCode;
+        private String uituCode;
+
+        public String getCertificateDocument() {
+            return certificateDocument;
+        }
+
+        public void setCertificateDocument(String certificateDocument) {
+            this.certificateDocument = certificateDocument;
+        }
+
+        public String getCertificateDocumentDate() {
+            return certificateDocumentDate;
+        }
+
+        public void setCertificateDocumentDate(String certificateDocumentDate) {
+            this.certificateDocumentDate = certificateDocumentDate;
+        }
+
+        public String getCertificateDocumentNumber() {
+            return certificateDocumentNumber;
+        }
+
+        public void setCertificateDocumentNumber(String certificateDocumentNumber) {
+            this.certificateDocumentNumber = certificateDocumentNumber;
+        }
+
+        public String getOwnerInn() {
+            return ownerInn;
+        }
+
+        public void setOwnerInn(String ownerInn) {
+            this.ownerInn = ownerInn;
+        }
+
+        public String getProducerInn() {
+            return producerInn;
+        }
+
+        public void setProducerInn(String producerInn) {
+            this.producerInn = producerInn;
+        }
+
+        public String getProductionDate() {
+            return productionDate;
+        }
+
+        public void setProductionDate(String productionDate) {
+            this.productionDate = productionDate;
+        }
+
+        public String getTnvedCode() {
+            return tnvedCode;
+        }
+
+        public void setTnvedCode(String tnvedCode) {
+            this.tnvedCode = tnvedCode;
+        }
+
+        public String getUitCode() {
+            return uitCode;
+        }
+
+        public void setUitCode(String uitCode) {
+            this.uitCode = uitCode;
+        }
+
+        public String getUituCode() {
+            return uituCode;
+        }
+
+        public void setUituCode(String uituCode) {
+            this.uituCode = uituCode;
+        }
+    }
+}
